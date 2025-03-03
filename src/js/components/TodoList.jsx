ea1071d66b03c100df06f3803fd1b1d4aee280ae
Jean Pierre Pluas <jampiier@gmail.com>
2025-03-03 21:02:52 +0000

    jampiier25

--- src/js/components/TodoList.jsx
+++ src/js/components/TodoList.jsx
@@ -5,19 +5,26 @@ const TodoList = () => {
   const [tasks, setTasks] = useState([]);
   const [input, setInput] = useState('');
 
-  //funcion para añadir
+  //función para agregar tarea
   const addTask = (task) => {
-    setTasks([...tasks, task]);
+    setTasks([...tasks, { text: task, completed: false }]);
     setInput('');
   };
 
-  //funcion para eliminar  
+  //función para eliminar tarea
   const deleteTask = (index) => {
     const updatedTasks = tasks.filter((_, i) => i !== index);
     setTasks(updatedTasks);
   };
 
-  //funcion para saber que se pulso enter
+  //función para elcheckbox
+  const toggleComplete = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index].completed = !updatedTasks[index].completed;
+    setTasks(updatedTasks);
+  };
+
+  //función para saber que se pulso enter
   const handleKeyPress = (event) => {
     if (event.key === 'Enter' && input.trim()) {
       addTask(input.trim());
@@ -25,33 +32,38 @@ const TodoList = () => {
   };
 
   return (
-    <div className='container'>
-      <h1>To Do List</h1>
+    <div className="container">
+      <h1>📝To Do List</h1>
 
       <input
         type="text"
-        placeholder="Add Some tasks"
+        placeholder="Add some Tasks!"
         value={input}
         onChange={(e) => setInput(e.target.value)}
         onKeyPress={handleKeyPress}
-      /> 
+        className="input-field"
+      />
 
-      <ul>
+      <ul className="task-list">
         {tasks.length === 0 ? (
           <li className="message">There are no tasks to complete</li>
         ) : (
           tasks.map((task, index) => (
-            <li key={index} className="task">
-              {task}     
-            <input
-                  type="checkbox"
-            />
+            <li
+              key={index}
+              className={`task ${task.completed ? 'completed' : ''}`}
+            >
+              <input
+                type="checkbox"
+                checked={task.completed}
+                onChange={() => toggleComplete(index)}
+              />
+              <span>{task.text}</span>
               <button
                 className="delete-btn"
                 onClick={() => deleteTask(index)}
-                
-              >❌ 
-
+              >
+                ❌
               </button>
             </li>
           ))
@@ -60,7 +72,7 @@ const TodoList = () => {
       <div className="task-counter">
         <span>{tasks.length === 0 ? '' : `You have ${tasks.length} item`}</span>
       </div>
-      </div>
+    </div>
   );
 };
 
